8b007dfbe2ba890a12f1d71e66e5d99e5aac7c1a
RodrigoMindera <rodrigo.diniz@mindera.com>
2024-11-09 10:25:13 +0000

    Refactoring

--- NorthWindDashBoard/views/charts.py
+++ NorthWindDashBoard/views/charts.py
@@ -6,14 +6,32 @@ from reflex.components.radix.themes.base import (
 )
 from reflex_ag_grid import ag_grid
 
-from ..models.customers import Customers, Order
+from ..models.customers import Customers
 
+
+column_defs = [
+    ag_grid.column_def(field="product_name", header_name="Product Name"),
+    ag_grid.column_def(
+        field="quantity", header_name="Quantity"
+    ),
+    ag_grid.column_def(
+        field="unit_price_formated", header_name="Price"
+    ),
+    ag_grid.column_def(
+        field="discount", header_name="Discount"
+    ),
+    ag_grid.column_def(
+        field="final_price", header_name="Total"
+    ),
+    ag_grid.column_def(
+        field="order_date", header_name="Order Date"
+    ),
+]
 
 class StatsState(rx.State):
     area_toggle: bool = True
     selected_tab: str = "revenue"
-    timeframe: str = "Monthly"
-    users_data = []
+    timeframe: str = "Monthly"    
     revenue_data = []
     orders_data = []    
     users_count:int
@@ -31,20 +49,32 @@ class StatsState(rx.State):
     loading_orders:bool=False
     df_order_details:pd.DataFrame=None  
 
+
+
+
+    def load_data_frames(self):
+      with rx.session() as session:
+        self.users_count = session.exec(func.count(Customers.customer_id)).scalar()                
+        self.df_orders = pd.read_sql("SELECT  * from orders",session.connection())
+        df = pd.read_sql('select * from order_details',session.connection())
+        self.df_merged= df.merge(self.df_orders)
+        self.df_order_details = pd.read_sql('SELECT o.order_date, od.order_id,p.product_name, od.quantity, od.unit_price, od.discount '
+                                            ' fROM order_details od join products p on od.product_id= p.product_id'\
+                                            ' join orders o on od.order_id = o.order_id',session.connection())       
+
     def toggle_areachart(self):
         self.area_toggle = not self.area_toggle
 
-    def load_data(self):      
+    def load_data(self):
+        self.load_data_frames()
+        
+        self.df_orders['YearMonth']= pd.to_datetime(self.df_orders['order_date']).dt.strftime('%Y-%m')
+        self.order_dates=self.df_orders['YearMonth'].unique().tolist()
+        self.order_date_selected=self.df_orders.iloc[0]['YearMonth']      
         self.loading_data=True       
-        with rx.session() as session:
-                self.users_count = session.exec(func.count(Customers.customer_id)).scalar()                
-                df_orders = pd.read_sql("SELECT  * from orders",session.connection())
-                df_orders['YearMonth']= pd.to_datetime(df_orders['order_date']).dt.strftime('%Y-%m')
-                self.order_dates=df_orders['YearMonth'].unique().tolist()
-                self.order_date_selected=df_orders.iloc[0]['YearMonth']
-                self.load_by_date()
-                self.loading_data=False
-                yield
+        self.load_by_date()
+        self.loading_data=False
+        yield
                 
     def on_change(self,ev): 
          self.loading_data=True        
@@ -57,57 +87,49 @@ class StatsState(rx.State):
        self.load_orders_table()
         
     def load_by_date(self):
-            with rx.session() as session:
-                if self.df_merged is None:
-                    df_orders = pd.read_sql('select * from orders',session.connection())
-                    df = pd.read_sql('select * from order_details',session.connection())
-                    self.df_merged= df.merge(df_orders)           
+        self.df_merged['YearMonth']= pd.to_datetime(self.df_merged['order_date']).dt.strftime('%Y-%m')
 
-                self.df_merged['YearMonth']= pd.to_datetime(self.df_merged['order_date']).dt.strftime('%Y-%m')
+        value= self.order_date_selected    
+        df_filtered=self.df_merged.query("YearMonth==@value").copy()
 
-                value= self.order_date_selected    
-                df_filtered=self.df_merged.query("YearMonth==@value").copy()
+        df_filtered['final_price']= df_filtered['unit_price'] * df_filtered['quantity']
+        self.total_sales= self.formatCurrency(df_filtered['final_price'].sum())       
 
-                df_filtered['final_price']= df_filtered['unit_price'] * df_filtered['quantity']
-                self.total_sales= df_filtered['final_price'].sum()             
+                    
+        self.orders_count=  len(df_filtered.groupby('order_id'))
+        
+        df_filtered['order_date']=df_filtered['order_date'].astype(str)
+       
+        result = df_filtered.groupby(['order_date'])['final_price'].sum().reset_index()               
+        
+        result = result.rename(columns={
+            'final_price': 'revenue'                   
+        })
+        self.revenue_data=result.to_dict(orient='records')
 
-                         
-                self.orders_count=  len(df_filtered.groupby('order_id'))
-                
-                df_filtered['order_date']=df_filtered['order_date'].astype(str)
-                # Step 5: Group by year-month and day, and sum the order amounts (Freight in this example)
-                result = df_filtered.groupby(['order_date'])['final_price'].sum().reset_index()               
+        orders_result= df_filtered.groupby(['order_date'])['order_id'].count().reset_index()
+        # number_of_orders
+        orders_result = orders_result.rename(columns={
+            'order_id': 'number_of_orders'                   
+        })
+        self.orders_data=orders_result.to_dict(orient='records')      
+
+        self.order_ids=df_filtered['order_id'].unique().tolist()
+        order_id_value= self.order_ids[0]
+        self.order_id_selected=order_id_value
+        self.load_orders_table()
                
-                result = result.rename(columns={
-                    'final_price': 'revenue'                   
-                })
-                self.revenue_data=result.to_dict(orient='records')
-
-                orders_result= df_filtered.groupby(['order_date'])['order_id'].count().reset_index()
-                # number_of_orders
-                orders_result = orders_result.rename(columns={
-                    'order_id': 'number_of_orders'                   
-                })
-                self.orders_data=orders_result.to_dict(orient='records')      
-
-                self.order_ids=df_filtered['order_id'].unique().tolist()
-                order_id_value= self.order_ids[0]
-                self.order_id_selected=order_id_value
-                self.load_orders_table()
-                #print(df_filtered.head())
-
+    def formatCurrency(self,curr):     
+            return"{:.1f}k".format(curr/1000)
         
     def load_orders_table(self):
         self.loading_orders=True        
         order_id= self.order_id_selected
-        with rx.session() as session:
-            #print(order_id)
-            if self.df_order_details is None:
-                self.df_order_details = pd.read_sql('SELECT od.order_id,p.product_name, od.quantity, od.unit_price, od.discount fROM order_details od join products p on od.product_id= p.product_id',session.connection())
-            self.df_order_details['final_price']= self.df_order_details['unit_price'] * self.df_order_details['quantity']
-            self.df_order_details['final_price'] = self.df_order_details['final_price'].apply(lambda x: "${:.1f}k".format((x/1000)))
-            self.orders_table_data =self.df_order_details.query("order_id==@order_id").copy().to_dict(orient='records')          
-            self.loading_orders=False
+        self.df_order_details['final_price']= self.df_order_details['unit_price'] * self.df_order_details['quantity']
+        self.df_order_details['unit_price_formated'] = self.df_order_details['unit_price'].apply(lambda x: "${:.1f}".format((x)))
+        self.df_order_details['final_price'] = self.df_order_details['final_price'].apply(lambda x: "${:.1f}".format((x)))
+        self.orders_table_data =self.df_order_details.query("order_id==@order_id").copy().to_dict(orient='records')          
+        self.loading_orders=False
            
 
 
@@ -157,7 +179,7 @@ def orders_table():
     return ag_grid(
         id="ag_grid_basic_2",
         row_data=StatsState.orders_table_data,
-        column_defs=[{"field": "product_name"},{"field": "quantity"}, {"field": "unit_price"}, {"field": "discount"},{"field":"final_price"}],
+        column_defs=column_defs,
         width="100%",
         height="40vh",
     )    
